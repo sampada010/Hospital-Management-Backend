71a36644b7a8f5634e24d1b6bb5cabc487c183f2
Sampada Tagalpallewar <tagalpallewarsampada10@gmail.com>
2025-03-31 11:44:21 +0530

    Added razorpay

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment } from '../controllers/userController.js';
+import { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 import upload from '../middlewares/multer.js';
 
@@ -15,6 +15,7 @@ userRouter.post('/book-appointment', authUser, bookAppointment);
 userRouter.post('/appointments', authUser, listAppointment); 
 userRouter.post('/cancel-appointment', authUser, cancelAppointment);
 
-// userRouter.post('/payment-razorpay', authUser, paymentRazorpay)
+userRouter.post('/payment-razorpay', authUser, paymentRazorpay)
+userRouter.post('/verify-razorpay', authUser, verifyRazorpay)
 
 export default userRouter;
