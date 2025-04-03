d436cd6c67ea75543fceb6067bce92add49bde09
Sampada Tagalpallewar <tagalpallewarsampada10@gmail.com>
2025-04-03 19:36:43 +0530

    ALL USERS API ADDED

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import upload from "../middlewares/multer.js";
-import { addDoctor, allDoctors, appointmentsAdmin, loginAdmin } from "../controllers/adminController.js";
+import { addDoctor, allDoctors, appointmentsAdmin, loginAdmin, allUsers } from "../controllers/adminController.js";
 import authAdmin from "../middlewares/authAdmin.js";
 
 const adminRouter = express.Router();
@@ -10,6 +10,7 @@ adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 
 adminRouter.get("/all-doctors", authAdmin, allDoctors);
+adminRouter.get("/all-users", allUsers);
 adminRouter.get("/appointments", authAdmin, appointmentsAdmin);
 
 export default adminRouter;
